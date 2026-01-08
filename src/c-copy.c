4d67465712fe4052be6f570fad96d72ee6d37b43
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2026-01-09 00:06:29 +0100

    Update c-copy.c

--- src/c-copy.c
+++ src/c-copy.c
@@ -4,12 +4,51 @@
 
 int main(int argc, char *argv[])
 {
-    return 0;
-}
+    // should support arguments: source path, dest path, thread count
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
 
+    if (argc < 3)
+    {
+        printf("Error: Not enough arguments supplied");
+        return 1;
+    }
+    // todo: read paths, check if exist
+
+    int threads = 4;
+    if (argc > 3)
+    {
+        threads = atoi(argv[3]);
+        // ensure reasonable number
+    }
+
+    return 0;
+
+    // explore src dir
+
+
+    // create multiple threads
+    // pthread_t thread;
+    // pthread_create(&thread, NULL, WorkerRoutine, NULL);
+
+    for (int i = 0; i < threads; i++) {
+        // create thread
+    }
+
+
+
+    // maybe also update progress percentage whenever a job is finished
+}
 
 int CreateJob()
 {
     CopyJob job = {"", "", 0};
     return 0;
 }
+
+
+void* WorkerRoutine(void* arg) {
+    printf("Created a new thread");
+    return NULL;
+ }
