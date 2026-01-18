c8b799e898a558d71182c41a4ad90803c15e29b0
Til-Weimann <weimanntil@web.de>
2026-01-18 19:11:21 +0100

    remove obsolete lines

--- tests/test_all.c
+++ tests/test_all.c
@@ -4,11 +4,6 @@
 
 int main(void)
 {
-
-    int i = MAX_JOBS;
-    printf("%i", i);
-    GetThreadCount(4, (const char *[]){"./c-copy", "src", "dest", "1", NULL});
-
     // From here, call the test methods of all test_subject.c files
     
     if (TestArgs() <= 0 && TestFiles() <= 0 && TestQueue() <= 0)
