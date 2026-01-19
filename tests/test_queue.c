84ca7c9685e868c2b55cba1c21fa2f8367b7a280
Landofranck <97963259+Landofranck@users.noreply.github.com>
2026-01-19 23:37:15 +0100

    Update test_queue.c
    queue test 1 and 2

--- tests/test_queue.c
+++ tests/test_queue.c
@@ -4,13 +4,44 @@
 
 int QueueTest1()
 {
-    // Implement some test here
+    JobQueue jq;
+    InitQueue(&jq);
+
+    CopyJob *job = malloc(sizeof(CopyJob));
+    if (!job) return -1;
+
+    job->fileSize = 123;
+
+    if (!Enqueue(&jq, job))
+    {
+        fprintf(stderr, "FAIL - enqueue failed\n");
+        return -2;
+    }
+
+    CopyJob *claimed = ClaimJob(&jq);
+    if (claimed != job)
+    {
+        fprintf(stderr, "FAIL - claimed job mismatch\n");
+        return -3;
+    }
+
+    free(claimed);
     return 0;
 }
 
+
 int QueueTest2()
 {
-    // Implement some test here
+    JobQueue jq;
+    InitQueue(&jq);
+
+    CopyJob *job = ClaimJob(&jq);
+    if (job != NULL)
+    {
+        fprintf(stderr, "FAIL - empty queue returned job\n");
+        return -1;
+    }
+
     return 0;
 }
 
